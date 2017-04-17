8a9801cb402ea9797404a86e0df1339b8e1ab67d
Administrator <Administrator@DESKTOP-NOJS0QA>
2017-04-17 14:37:53 +0800

    添加注释

--- src/main/java/com/cpthack/commons/ehcache/core/CacheClient.java
+++ src/main/java/com/cpthack/commons/ehcache/core/CacheClient.java
@@ -23,7 +23,7 @@ import com.cpthack.commons.ehcache.config.EhcacheConfig;
  * <b>CacheClient.java</b></br>
  * 
  * <pre>
- * TODO(这里用一句话描述这个类的作用)
+ * Ehcache操作工具API定义
  * </pre>
  *
  * @author cpthack cpt@jianzhimao.com
@@ -32,19 +32,128 @@ import com.cpthack.commons.ehcache.config.EhcacheConfig;
  */
 public interface CacheClient {
 	
+	/**
+	 * 
+	 * <b>setEhcacheConfig</b> <br/>
+	 * <br/>
+	 * 
+	 * 自定义EhcacheConfig配置类,如果不定义的话，则使用类库中默认配置<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @param ehcacheConfig
+	 * @return CacheClient
+	 *
+	 */
 	CacheClient setEhcacheConfig(EhcacheConfig ehcacheConfig);
 	
+	/**
+	 * 
+	 * <b>getCacheManage</b> <br/>
+	 * <br/>
+	 * 
+	 * 获取Ehcache操作类对象：CacheManager<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @return CacheManager
+	 *
+	 */
 	CacheManager getCacheManage();
 	
+	/**
+	 * 
+	 * <b>get</b> <br/>
+	 * <br/>
+	 * 
+	 * 从缓存中读取数据 <br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @param orgin
+	 *            缓存名称
+	 * @param key
+	 *            具体缓存KEY
+	 * @return String
+	 *
+	 */
 	String get(String orgin, String key);
 	
+	/**
+	 * 
+	 * <b>put</b> <br/>
+	 * <br/>
+	 * 
+	 * 添加数据到缓存中<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @param orgin
+	 *            缓存名称
+	 * @param key
+	 *            具体缓存KEY
+	 * @param value
+	 *            具体缓存值
+	 * @return boolean
+	 *
+	 */
 	boolean put(String orgin, String key, String value);
 	
+	/**
+	 * 
+	 * <b>delete</b> <br/>
+	 * <br/>
+	 * 
+	 * 从缓存中删除数据<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @param orgin
+	 *            缓存名称
+	 * @param key
+	 *            具体缓存KEY
+	 * @return boolean
+	 *
+	 */
 	boolean delete(String orgin, String key);
 	
+	/**
+	 * 
+	 * <b>update</b> <br/>
+	 * <br/>
+	 * 
+	 * 从缓存中更新数据<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @param orgin
+	 *            缓存名称
+	 * @param key
+	 *            具体缓存KEY
+	 * @param value
+	 *            具体缓存值
+	 * @return boolean
+	 *
+	 */
 	boolean update(String orgin, String key, String value);
 	
+	/**
+	 * 
+	 * <b>start</b> <br/>
+	 * <br/>
+	 * 
+	 * 初始化Ehcache操作类对象：CacheManager，此处才是真正生成CacheManager的时候<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @return boolean
+	 *
+	 */
 	boolean start();
 	
+	/**
+	 * 
+	 * <b>stop</b> <br/>
+	 * <br/>
+	 * 
+	 * 关闭Ehcache操作类对象：CacheManager。内部实际调用CacheManager.stop();<br/>
+	 * 
+	 * @author cpthack cpt@jianzhimao.com
+	 * @return boolean
+	 *
+	 */
 	boolean stop();
 }
